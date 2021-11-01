8364b2d4b43b9014131a33667cbb6ebaa0554f49
adityatriand <adityatriananda0@gmail.com>
2021-11-02 02:05:48 +0700

    add form cari laptop & send form cari to page hasil

--- laptop_determination/mylaptop/views.py
+++ laptop_determination/mylaptop/views.py
@@ -1,5 +1,5 @@
-from django.core import serializers
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from . import models
 
 # Create your views here.
@@ -12,7 +12,7 @@ def alternatif(request):
 
 def cari(request):
     data = models.Laptops.objects.all().order_by('company')
-    criterions = ['Pilih Kepentingan','Harga','RAM','CPU','SSD','HDD']
+    criterions = ['Default','Harga','RAM','CPU','SSD','HDD']
     unique_brands = []
     brands = []
     for x in data:
@@ -23,10 +23,25 @@ def cari(request):
 
     context = {
         'brands': unique_brands,
-        'criterias':criterions
+        'criterias':criterions,
+        'cari': data
     }
 
     return render(request, 'mylaptop/cari.html', context)
 
 def hasil(request):
-    return render(request, 'mylaptop/hasil.html')
+
+    data = {}
+    if request.method == 'POST':
+        data['brand'] = request.POST['brand']
+        for i in range(5):
+            data['penting'+str(i+1)] = request.POST['penting_'+str(i+1)]
+
+        context = {
+            'cari': data
+        }
+
+        return render(request, 'mylaptop/hasil.html', context)
+
+    else:
+        return HttpResponseRedirect('/mylaptop/cari/')
