92c9c324dd1d1ea18caae32a11672e8fe833ee5a
adityatriand <adityatriananda0@gmail.com>
2021-11-01 23:20:44 +0700

    update content from adit

--- laptop_determination/mylaptop/views.py
+++ laptop_determination/mylaptop/views.py
@@ -1,3 +1,4 @@
+from django.core import serializers
 from django.shortcuts import render
 from . import models
 
@@ -10,7 +11,22 @@ def alternatif(request):
     return render(request, 'mylaptop/alternatif.html', context)
 
 def cari(request):
-    return render(request, 'mylaptop/cari.html')
+    data = models.Laptops.objects.all().order_by('company')
+    criterions = ['Pilih Kepentingan','Harga','RAM','CPU','SSD','HDD']
+    unique_brands = []
+    brands = []
+    for x in data:
+        # check if exists in unique_brands or not
+        if x.company not in brands:
+            unique_brands.append(x)
+            brands.append(x.company)
+
+    context = {
+        'brands': unique_brands,
+        'criterias':criterions
+    }
+
+    return render(request, 'mylaptop/cari.html', context)
 
 def hasil(request):
     return render(request, 'mylaptop/hasil.html')
